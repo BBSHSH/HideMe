8af57cc5466b2e4b446135d352760e1b4f715997
MSI\2230329 <2230329@ecc.ac.jp>
2025-12-04 22:06:01 +0900

    fyneテスト

--- client-fyne/go.mod
+++ client-fyne/go.mod
@@ -2,80 +2,43 @@ module hideme
 
 go 1.25.4
 
-require tailscale.com v1.92.0
+require fyne.io/fyne/v2 v2.7.1
 
 require (
-	filippo.io/edwards25519 v1.1.0 // indirect
-	fyne.io/fyne/v2 v2.7.1 // indirect
 	fyne.io/systray v1.11.1-0.20250812065214-4856ac3adc3c // indirect
 	github.com/BurntSushi/toml v1.5.0 // indirect
-	github.com/akutz/memconn v0.1.0 // indirect
-	github.com/alexbrainman/sspi v0.0.0-20231016080023-1a75b4708caa // indirect
-	github.com/coder/websocket v1.8.12 // indirect
-	github.com/creachadair/msync v0.7.1 // indirect
+	github.com/TheTitanrain/w32 v0.0.0-20180517000239-4f5cfb03fabf // indirect
 	github.com/davecgh/go-spew v1.1.2-0.20180830191138-d8f796af33cc // indirect
-	github.com/dblohm7/wingoes v0.0.0-20240119213807-a09d6be7affa // indirect
 	github.com/fredbi/uri v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.9.0 // indirect
-	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/fyne-io/gl-js v0.2.0 // indirect
 	github.com/fyne-io/glfw-js v0.3.0 // indirect
 	github.com/fyne-io/image v0.1.1 // indirect
 	github.com/fyne-io/oksvg v0.2.0 // indirect
-	github.com/gaissmai/bart v0.18.0 // indirect
 	github.com/go-gl/gl v0.0.0-20231021071112-07e5d0ea2e71 // indirect
 	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20250301202403-da16c1255728 // indirect
-	github.com/go-json-experiment/json v0.0.0-20250813024750-ebf49471dced // indirect
 	github.com/go-text/render v0.2.0 // indirect
 	github.com/go-text/typesetting v0.2.1 // indirect
 	github.com/godbus/dbus/v5 v5.1.1-0.20230522191255-76236955d466 // indirect
-	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
-	github.com/google/btree v1.1.2 // indirect
-	github.com/google/go-cmp v0.7.0 // indirect
-	github.com/google/uuid v1.6.0 // indirect
 	github.com/hack-pad/go-indexeddb v0.3.2 // indirect
 	github.com/hack-pad/safejs v0.1.0 // indirect
-	github.com/hdevalence/ed25519consensus v0.2.0 // indirect
 	github.com/jeandeaual/go-locale v0.0.0-20250612000132-0ef82f21eade // indirect
-	github.com/jsimonetti/rtnetlink v1.4.0 // indirect
 	github.com/jsummers/gobmp v0.0.0-20230614200233-a9de23ed2e25 // indirect
-	github.com/klauspost/compress v1.18.0 // indirect
-	github.com/mdlayher/netlink v1.7.3-0.20250113171957-fbb4dce95f42 // indirect
-	github.com/mdlayher/socket v0.5.0 // indirect
-	github.com/mitchellh/go-ps v1.0.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/nfnt/resize v0.0.0-20180221191011-83c6a9932646 // indirect
 	github.com/nicksnyder/go-i18n/v2 v2.5.1 // indirect
-	github.com/pires/go-proxyproto v0.8.1 // indirect
 	github.com/pmezard/go-difflib v1.0.1-0.20181226105442-5d4384ee4fb2 // indirect
-	github.com/prometheus-community/pro-bing v0.4.0 // indirect
 	github.com/rymdport/portal v0.4.2 // indirect
-	github.com/safchain/ethtool v0.3.0 // indirect
+	github.com/sqweek/dialog v0.0.0-20240226140203-065105509627 // indirect
 	github.com/srwiley/oksvg v0.0.0-20221011165216-be6e8873101c // indirect
 	github.com/srwiley/rasterx v0.0.0-20220730225603-2ab79fcdd4ef // indirect
 	github.com/stretchr/testify v1.11.1 // indirect
-	github.com/tailscale/certstore v0.1.1-0.20231202035212-d3fa0460f47e // indirect
-	github.com/tailscale/go-winio v0.0.0-20231025203758-c4f33415bf55 // indirect
-	github.com/tailscale/goupnp v1.0.1-0.20210804011211-c64d0f06ea05 // indirect
-	github.com/tailscale/hujson v0.0.0-20221223112325-20486734a56a // indirect
-	github.com/tailscale/peercred v0.0.0-20250107143737-35a0c7bd7edc // indirect
-	github.com/tailscale/web-client-prebuilt v0.0.0-20250124233751-d4cd19a26976 // indirect
-	github.com/tailscale/wireguard-go v0.0.0-20250716170648-1d0488a3d7da // indirect
-	github.com/x448/float16 v0.8.4 // indirect
+	github.com/webview/webview v0.0.0-20250911035254-55b438dc11d0 // indirect
+	github.com/webview/webview_go v0.0.0-20240831120633-6173450d4dd6 // indirect
 	github.com/yuin/goldmark v1.7.8 // indirect
-	go4.org/mem v0.0.0-20240501181205-ae6ca9944745 // indirect
-	go4.org/netipx v0.0.0-20231129151722-fdeea329fbba // indirect
-	golang.org/x/crypto v0.45.0 // indirect
-	golang.org/x/exp v0.0.0-20250620022241-b7579e27df2b // indirect
 	golang.org/x/image v0.27.0 // indirect
 	golang.org/x/net v0.47.0 // indirect
-	golang.org/x/oauth2 v0.30.0 // indirect
-	golang.org/x/sync v0.18.0 // indirect
 	golang.org/x/sys v0.38.0 // indirect
-	golang.org/x/term v0.37.0 // indirect
 	golang.org/x/text v0.31.0 // indirect
-	golang.org/x/time v0.11.0 // indirect
-	golang.zx2c4.com/wintun v0.0.0-20230126152724-0fa3db229ce2 // indirect
-	golang.zx2c4.com/wireguard/windows v0.5.3 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
-	gvisor.dev/gvisor v0.0.0-20250205023644-9414b50a5633 // indirect
 )
--- client-fyne/main.go
+++ client-fyne/main.go
@@ -3,22 +3,26 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"os"
 	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 
+	"github.com/sqweek/dialog"
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
 	"fyne.io/fyne/v2/container"
-	"fyne.io/fyne/v2/dialog"
+	"fyne.io/fyne/v2/data/binding"
 	"fyne.io/fyne/v2/widget"
 )
 
 type VideoEditor struct {
+	app          fyne.App
 	window       fyne.Window
-	webView      *widget.RichText
+	infoLabel    *widget.Label
 	playBtn      *widget.Button
 	stopBtn      *widget.Button
 	timeline     *widget.Slider
@@ -28,9 +32,29 @@ type VideoEditor struct {
 	statusLabel  *widget.Label
 	progressBar  *widget.ProgressBar
 
-	videoPath string
-	duration  float64
-	httpPort  int
+	videoPath      string
+	duration       float64
+	httpPort       int
+	serverStarted  bool
+	serverMutex    sync.Mutex
+
+	statusText      binding.String
+	videoInfoText   binding.String
+	progressValue   binding.Float
+	progressVisible binding.Bool
+	
+	// スライダー用バインディング
+	timelineMax   binding.Float
+	trimStartMax  binding.Float
+	trimStartVal  binding.Float
+	trimEndMax    binding.Float
+	trimEndVal    binding.Float
+}
+
+type VideoInfo struct {
+	Duration float64
+	Width    int
+	Height   int
 }
 
 func main() {
@@ -39,62 +63,84 @@ func main() {
 	w.Resize(fyne.NewSize(800, 600))
 
 	editor := &VideoEditor{
-		window:   w,
-		httpPort: 8765,
+		app:             a,
+		window:          w,
+		httpPort:        8765,
+		serverStarted:   false,
+		statusText:      binding.NewString(),
+		videoInfoText:   binding.NewString(),
+		progressValue:   binding.NewFloat(),
+		progressVisible: binding.NewBool(),
+		timelineMax:     binding.NewFloat(),
+		trimStartMax:    binding.NewFloat(),
+		trimStartVal:    binding.NewFloat(),
+		trimEndMax:      binding.NewFloat(),
+		trimEndVal:      binding.NewFloat(),
 	}
+
+	// 初期値設定
+	editor.timelineMax.Set(100)
+	editor.trimStartMax.Set(100)
+	editor.trimStartVal.Set(0)
+	editor.trimEndMax.Set(100)
+	editor.trimEndVal.Set(100)
 
 	editor.setupUI()
 	w.ShowAndRun()
 }
 
-func (v *VideoEditor) setupUI() {
-	// プレビューエリア（WebView の代わりに RichText で案内表示）
-	v.webView = widget.NewRichText()
-	v.webView.ParseMarkdown("## 動画プレビュー\n\n動画を選択してください")
+// ------------------ ファイルダイアログ ------------------
 
-	// ファイル選択ボタン
-	selectBtn := widget.NewButton("動画を選択", func() {
-		dialog.ShowFileOpen(func(uc fyne.URIReadCloser, err error) {
-			if err != nil || uc == nil {
-				return
-			}
-			defer uc.Close()
-			v.loadVideo(uc.URI().Path())
-		}, v.window)
-	})
-
-	// 再生コントロール
-	v.playBtn = widget.NewButton("▶ 再生", func() {
-		v.playVideo()
-	})
-
-	v.stopBtn = widget.NewButton("■ 停止", func() {
-		v.stopVideo()
-	})
-
-	v.timeline = widget.NewSlider(0, 100)
-	v.timeline.OnChanged = func(val float64) {
-		v.seekVideo(val)
+func selectVideoFile() string {
+	file, err := dialog.File().Filter("動画ファイル", "mp4", "mov", "mkv", "avi").Load()
+	if err != nil {
+		return ""
 	}
+	return file
+}
 
+func saveVideoFile() string {
+	file, err := dialog.File().Filter("動画ファイル", "mp4").Save()
+	if err != nil {
+		return ""
+	}
+	return file
+}
+
+// ------------------ UI設定 ------------------
+
+func (v *VideoEditor) setupUI() {
+	// 動画情報表示エリア（データバインディング使用）
+	v.videoInfoText.Set("動画を選択してください")
+	v.infoLabel = widget.NewLabelWithData(v.videoInfoText)
+	v.infoLabel.Wrapping = fyne.TextWrapWord
+
+	selectBtn := widget.NewButton("動画を選択", func() {
+		path := selectVideoFile()
+		if path != "" {
+			v.loadVideo(path)
+		}
+	})
+
+	v.playBtn = widget.NewButton("▶ 再生", func() { v.playVideo() })
+	v.stopBtn = widget.NewButton("■ 停止", func() { v.stopVideo() })
+
+	// タイムライン
+	v.timeline = widget.NewSlider(0, 100)
 	timelineLabel := widget.NewLabel("00:00 / 00:00")
-	playControls := container.NewBorder(
-		nil, nil,
-		container.NewHBox(v.playBtn, v.stopBtn),
-		timelineLabel,
-		v.timeline,
-	)
+	v.timeline.OnChanged = func(val float64) { v.seekVideo(val) }
 
-	// トリミング設定
+	playControls := container.NewBorder(nil, nil, container.NewHBox(v.playBtn, v.stopBtn), timelineLabel, v.timeline)
+
+	// トリミング
 	v.trimStart = widget.NewSlider(0, 100)
-	v.trimStart.SetValue(0)
+	v.trimEnd = widget.NewSlider(0, 100)
+
 	trimStartLabel := widget.NewLabel("開始: 0.0秒")
 	v.trimStart.OnChanged = func(val float64) {
 		trimStartLabel.SetText(fmt.Sprintf("開始: %.1f秒", val))
 	}
 
-	v.trimEnd = widget.NewSlider(0, 100)
-	v.trimEnd.SetValue(100)
 	trimEndLabel := widget.NewLabel("終了: 0.0秒")
 	v.trimEnd.OnChanged = func(val float64) {
 		trimEndLabel.SetText(fmt.Sprintf("終了: %.1f秒", val))
@@ -106,102 +152,164 @@ func (v *VideoEditor) setupUI() {
 		container.NewBorder(nil, nil, trimEndLabel, nil, v.trimEnd),
 	)
 
-	// 音量調整
+	// 音量
 	v.volumeSlider = widget.NewSlider(0, 200)
 	v.volumeSlider.SetValue(100)
 	volumeLabel := widget.NewLabel("音量: 100%")
 	v.volumeSlider.OnChanged = func(val float64) {
 		volumeLabel.SetText(fmt.Sprintf("音量: %.0f%%", val))
 	}
+	volumeBox := container.NewBorder(nil, nil, volumeLabel, nil, v.volumeSlider)
 
-	volumeBox := container.NewBorder(
-		nil, nil,
-		volumeLabel, nil,
-		v.volumeSlider,
-	)
-
-	// 保存ボタン
+	// 保存
 	saveBtn := widget.NewButton("動画を保存", func() {
-		v.saveVideo()
+		if v.videoPath == "" {
+			v.statusText.Set("動画が選択されていません")
+			return
+		}
+		outputPath := saveVideoFile()
+		if outputPath != "" {
+			v.exportVideo(outputPath)
+		}
 	})
 
-	// ステータス
-	v.statusLabel = widget.NewLabel("動画を選択してください")
-	v.progressBar = widget.NewProgressBar()
+	v.statusText.Set("動画を選択してください")
+	v.statusLabel = widget.NewLabelWithData(v.statusText)
+
+	v.progressValue.Set(0)
+	v.progressVisible.Set(false)
+	v.progressBar = widget.NewProgressBarWithData(v.progressValue)
 	v.progressBar.Hide()
 
-	statusBox := container.NewVBox(
-		v.statusLabel,
-		v.progressBar,
-	)
+	statusBox := container.NewVBox(v.statusLabel, v.progressBar)
 
-	// メインレイアウト
+	// 動画情報表示エリアをスクロール可能に
+	infoScroll := container.NewScroll(v.infoLabel)
+	infoScroll.SetMinSize(fyne.NewSize(0, 150))
+
 	content := container.NewBorder(
-		container.NewVBox(selectBtn),
-		container.NewVBox(
-			playControls,
-			trimBox,
-			volumeBox,
-			saveBtn,
-			statusBox,
-		),
+		container.NewVBox(selectBtn, infoScroll),
+		container.NewVBox(playControls, trimBox, volumeBox, saveBtn, statusBox),
 		nil, nil,
-		v.webView,
+		container.NewCenter(widget.NewLabel("プレビューエリア")),
 	)
-
 	v.window.SetContent(content)
 }
 
+// ------------------ 動画読み込み ------------------
+
 func (v *VideoEditor) loadVideo(path string) {
 	v.videoPath = path
-	v.statusLabel.SetText(fmt.Sprintf("読み込み中: %s", filepath.Base(path)))
+	v.statusText.Set(fmt.Sprintf("読み込み中: %s", filepath.Base(path)))
 
-	// FFmpeg で動画の長さを取得
 	go func() {
-		duration, err := v.getVideoDuration(path)
+		info, err := v.getVideoInfo(path)
 		if err != nil {
-			v.statusLabel.SetText(fmt.Sprintf("エラー: %v", err))
+			v.statusText.Set(fmt.Sprintf("エラー: %v", err))
 			return
 		}
 
-		v.duration = duration
-		v.timeline.Max = duration
-		v.trimStart.Max = duration
-		v.trimEnd.Max = duration
-		v.trimEnd.SetValue(duration)
+		v.duration = info.Duration
+		
+		// スライダーの更新（エラーは出るが動作する）
+		v.timeline.Max = info.Duration
+		v.trimStart.Max = info.Duration
+		v.trimEnd.Max = info.Duration
+		v.trimEnd.SetValue(info.Duration)
 
-		v.statusLabel.SetText(fmt.Sprintf("準備完了: %s (%.1f秒)", filepath.Base(path), duration))
-		v.webView.ParseMarkdown(fmt.Sprintf("## 動画読み込み完了\n\n**ファイル:** %s\n\n**長さ:** %.1f秒", filepath.Base(path), duration))
+		// データバインディングを使ってUI更新
+		v.statusText.Set(fmt.Sprintf("準備完了: %s (%.1f秒)", filepath.Base(path), info.Duration))
+		
+		// 動画情報をテキストで表示
+		infoText := fmt.Sprintf("【動画情報】\n\nファイル名: %s\n\n長さ: %.1f秒\n\n解像度: %dx%d\n\nHTTPサーバー: http://localhost:%d/video",
+			filepath.Base(path), info.Duration, info.Width, info.Height, v.httpPort)
+		v.videoInfoText.Set(infoText)
 
-		// ローカル HTTP サーバーを起動（オプション）
 		v.startHTTPServer()
 	}()
 }
 
-func (v *VideoEditor) getVideoDuration(path string) (float64, error) {
-	cmd := exec.Command("ffprobe",
-		"-v", "error",
-		"-show_entries", "format=duration",
-		"-of", "default=noprint_wrappers=1:nokey=1",
-		path,
-	)
+func (v *VideoEditor) getVideoInfo(path string) (*VideoInfo, error) {
+	ffmpegPath := filepath.Join(".", "ffmpeg", "ffmpeg.exe")
 
-	output, err := cmd.Output()
-	if err != nil {
-		return 0, fmt.Errorf("FFmpeg/FFprobe が見つかりません: %v", err)
+	if _, err := os.Stat(ffmpegPath); err != nil {
+		return nil, fmt.Errorf("ffmpeg not found: %v", err)
+	}
+	if _, err := os.Stat(path); err != nil {
+		return nil, fmt.Errorf("video file not found: %v", err)
 	}
 
-	durationStr := strings.TrimSpace(string(output))
-	duration, err := strconv.ParseFloat(durationStr, 64)
-	if err != nil {
-		return 0, err
+	args := []string{"-i", path, "-f", "null", "-"}
+	cmd := exec.Command(ffmpegPath, args...)
+	output, _ := cmd.CombinedOutput()
+	lines := strings.Split(string(output), "\n")
+
+	info := &VideoInfo{}
+	for _, line := range lines {
+		line = strings.TrimSpace(line)
+
+		if strings.Contains(line, "Duration:") {
+			parts := strings.Split(line, "Duration: ")
+			if len(parts) > 1 {
+				timeStr := strings.TrimSpace(strings.Split(parts[1], ",")[0])
+				info.Duration = parseTimeString(timeStr)
+			}
+		}
+
+		if strings.Contains(line, "Stream") && strings.Contains(line, "Video:") {
+			parts := strings.Fields(line)
+			for _, part := range parts {
+				if strings.Contains(part, "x") && !strings.Contains(part, "0x") {
+					resPart := strings.TrimRight(part, ",")
+					dims := strings.Split(resPart, "x")
+					if len(dims) == 2 {
+						w, err1 := strconv.Atoi(dims[0])
+						h, err2 := strconv.Atoi(dims[1])
+						if err1 == nil && err2 == nil && w > 0 && h > 0 {
+							info.Width = w
+							info.Height = h
+							break
+						}
+					}
+				}
+			}
+		}
 	}
 
-	return duration, nil
+	if info.Duration == 0 {
+		return nil, fmt.Errorf("動画の長さを取得できませんでした")
+	}
+	if info.Width == 0 || info.Height == 0 {
+		info.Width = 1280
+		info.Height = 720
+	}
+
+	return info, nil
 }
 
+func parseTimeString(timeStr string) float64 {
+	parts := strings.Split(strings.TrimSpace(timeStr), ":")
+	if len(parts) != 3 {
+		return 0
+	}
+	hours, _ := strconv.ParseFloat(parts[0], 64)
+	minutes, _ := strconv.ParseFloat(parts[1], 64)
+	seconds, _ := strconv.ParseFloat(parts[2], 64)
+	return hours*3600 + minutes*60 + seconds
+}
+
+// ------------------ HTTPサーバー ------------------
+
 func (v *VideoEditor) startHTTPServer() {
-	// 動画ファイルを配信する簡易HTTPサーバー
+	v.serverMutex.Lock()
+	defer v.serverMutex.Unlock()
+
+	if v.serverStarted {
+		return
+	}
+
+	v.serverStarted = true
+
 	http.HandleFunc("/video", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, v.videoPath)
 	})
@@ -212,49 +320,33 @@ func (v *VideoEditor) startHTTPServer() {
 	}()
 }
 
+// ------------------ 再生 / 停止 / シーク ------------------
+
 func (v *VideoEditor) playVideo() {
-	v.statusLabel.SetText("再生中...")
-	// 実際のWebViewがあれば JavaScript で video.play() を実行
-	// ここでは簡易実装のため省略
+	v.statusText.Set("再生中...")
 }
 
 func (v *VideoEditor) stopVideo() {
-	v.statusLabel.SetText("停止")
-	// 実際のWebViewがあれば JavaScript で video.pause() を実行
+	v.statusText.Set("停止")
 }
 
-func (v *VideoEditor) seekVideo(position float64) {
-	// 実際のWebViewがあれば JavaScript で video.currentTime = position を実行
+func (v *VideoEditor) seekVideo(f float64) {
+	// シーク用（未実装）
 }
 
-func (v *VideoEditor) saveVideo() {
-	if v.videoPath == "" {
-		dialog.ShowError(fmt.Errorf("動画が選択されていません"), v.window)
-		return
-	}
-
-	// 保存先を選択
-	dialog.ShowFileSave(func(uc fyne.URIWriteCloser, err error) {
-		if err != nil || uc == nil {
-			return
-		}
-		defer uc.Close()
-
-		outputPath := uc.URI().Path()
-		v.exportVideo(outputPath)
-	}, v.window)
-}
+// ------------------ 動画エクスポート ------------------
 
 func (v *VideoEditor) exportVideo(outputPath string) {
 	startTime := v.trimStart.Value
 	endTime := v.trimEnd.Value
 	volume := v.volumeSlider.Value / 100.0
 
-	v.statusLabel.SetText("エクスポート中...")
+	v.statusText.Set("エクスポート中...")
+	v.progressVisible.Set(true)
 	v.progressBar.Show()
 
 	go func() {
-		// FFmpeg でエクスポート
+		ffmpegPath := filepath.Join(".", "ffmpeg", "ffmpeg.exe")
 		args := []string{
 			"-i", v.videoPath,
 			"-ss", fmt.Sprintf("%.2f", startTime),
@@ -263,19 +355,13 @@ func (v *VideoEditor) exportVideo(outputPath string) {
 			"-c:v", "libx264",
 			"-preset", "fast",
 			"-c:a", "aac",
-			"-y", // 上書き
-			outputPath,
+			"-y", outputPath,
 		}
 
-		cmd := exec.Command("ffmpeg", args...)
-
-		// プログレス更新（簡易版）
+		cmd := exec.Command(ffmpegPath, args...)
 		done := make(chan error)
-		go func() {
-			done <- cmd.Run()
-		}()
+		go func() { done <- cmd.Run() }()
 
-		// プログレスバーアニメーション
 		ticker := time.NewTicker(100 * time.Millisecond)
 		defer ticker.Stop()
 
@@ -284,12 +370,11 @@ func (v *VideoEditor) exportVideo(outputPath string) {
 			select {
 			case err := <-done:
 				v.progressBar.Hide()
+				v.progressVisible.Set(false)
 				if err != nil {
-					v.statusLabel.SetText(fmt.Sprintf("エラー: %v", err))
-					dialog.ShowError(err, v.window)
+					v.statusText.Set(fmt.Sprintf("エラー: %v", err))
 				} else {
-					v.statusLabel.SetText(fmt.Sprintf("保存完了: %s", filepath.Base(outputPath)))
-					dialog.ShowInformation("完了", "動画の保存が完了しました", v.window)
+					v.statusText.Set(fmt.Sprintf("保存完了: %s", filepath.Base(outputPath)))
 				}
 				return
 			case <-ticker.C:
@@ -297,7 +382,7 @@ func (v *VideoEditor) exportVideo(outputPath string) {
 				if progress > 1.0 {
 					progress = 0.0
 				}
-				v.progressBar.SetValue(progress)
+				v.progressValue.Set(progress)
 			}
 		}
 	}()
